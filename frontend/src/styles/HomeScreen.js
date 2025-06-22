a8a65142ce3bc62d5b7b8a5a8567b3ca9a5cc415
John Xereas <dcf9rp@virginia.edu>
2025-06-22 11:18:51 -0400

    Added: the Home Screen

---
+++ frontend/src/styles/HomeScreen.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#04060c",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+  },
+  gradientContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "500",
+    color: "#d385b3",
+    marginBottom: 40,
+  },
+  titleBold: {
+    fontWeight: "900",
+  },
+  logo: {
+    width: 257,
+    height: 257,
+    marginBottom: 40,
+    resizeMode: "contain",
+  },
+  subtitle: {
+    fontSize: 18,
+    color: "#7c7493",
+    marginBottom: 2,
+  },
+  button: {
+    backgroundColor: "#812050",
+    borderColor: "#d385b3",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    marginVertical: 8,
+    width: "80%",
+    alignItems: "center",
+    borderRadius: 15,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "400",
+  },
+});
