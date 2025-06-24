36c3b72ed1d6984ba397040d08d5d850ee1ba3d4
John Xereas <dcf9rp@virginia.edu>
2025-06-24 15:14:41 -0400

    Added: bottom navigation bar

---
+++ frontend/src/styles/BottomNavBar.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#0e0d16",
+    paddingVertical: 12,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    borderTopWidth: 1,
+    height: 69,
+    borderTopColor: "#292840",
+  },
+  navItem: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
