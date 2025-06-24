dc34ba4302dc27b49b2ab49861d473e5e5284b14
John Xereas <dcf9rp@virginia.edu>
2025-06-24 15:15:10 -0400

    Added: bottom navigation bar frontend

---
+++ frontend/src/components/BottomNavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, TouchableOpacity } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import styles from "../styles/BottomNavBar";
+
+const BottomNavBar = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const currentRoute = route.name;
+
+  const tabs = [
+    { name: "Dashboard", icon: "home" },
+    { name: "Goals", icon: "bullseye" },
+    { name: "Tasks", icon: "check-square" },
+    { name: "Profile", icon: "user" },
+  ];
+
+  return (
+    <View style={styles.container}>
+      {tabs.map((tab, index) => (
+        <TouchableOpacity
+          key={index}
+          style={styles.navItem}
+          onPress={() => navigation.navigate(tab.name)}
+        >
+          <Icon
+            name={tab.icon}
+            size={24}
+            color={currentRoute === tab.name ? "#cf59a9" : "#7c7493"}
+          />
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+
+export default BottomNavBar;
