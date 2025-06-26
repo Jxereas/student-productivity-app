49d634ad330c18326c65a88e85e621d6a278cfeb
John Xereas <dcf9rp@virginia.edu>
2025-06-26 13:53:17 -0400

    Added: tasks main screen

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   storageBucket: "student-productivity-app-a5e28.firebasestorage.app",
   messagingSenderId: "783085439265",
   appId: "1:783085439265:web:20eda210403a51ecbf5660"
-
 };
 
 const app = initializeApp(firebaseConfig);
