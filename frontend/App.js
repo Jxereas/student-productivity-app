fbb29b8f7132121b94debe19f19bf4172d22ebb2
John Xereas <dcf9rp@virginia.edu>
2025-06-30 14:09:40 -0400

    Added: login screen

--- frontend/App.js
+++ frontend/App.js
@@ -3,23 +3,30 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/components/HomeScreen";
 import SignUp from "./src/components/SignUp";
+import Login from "./src/components/Login";
 import TasksMainScreen from "./src/components/TasksMainScreen";
 import OverdueTasksScreen from "./src/components/TasksOverdueScreen";
+import ForgotPassword from "./src/components/ForgotPassword";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="TasksMainScreen" component={TasksMainScreen} />
-        <Stack.Screen name="OverdueTasksScreen" component={OverdueTasksScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Login"
+                screenOptions={{ headerShown: false }}
+            >
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="SignUp" component={SignUp} />
+                <Stack.Screen name="TasksMainScreen" component={TasksMainScreen} />
+                <Stack.Screen
+                    name="OverdueTasksScreen"
+                    component={OverdueTasksScreen}
+                />
+                <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
