62035133329c5db1310bc25edf8cc45c7d3be4a8
John Xereas <dcf9rp@virginia.edu>
2025-07-02 14:46:26 -0400

    Added: import for goals main screen to App.js

--- frontend/App.js
+++ frontend/App.js
@@ -9,29 +9,31 @@ import TasksMainScreen from "./src/components/TasksMainScreen";
 import OverdueTasksScreen from "./src/components/TasksOverdueScreen";
 import ForgotPassword from "./src/components/ForgotPassword";
 import AddTask from "./src/components/AddTask";
+import Goals from "./src/components/Goals";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <AlertProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="AddTask"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-          <Stack.Screen name="TasksMainScreen" component={TasksMainScreen} />
-          <Stack.Screen
-            name="OverdueTasksScreen"
-            component={OverdueTasksScreen}
-          />
-          <Stack.Screen name="AddTask" component={AddTask} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AlertProvider>
-  );
+    return (
+        <AlertProvider>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="Goals"
+                    screenOptions={{ headerShown: false }}
+                >
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Login" component={Login} />
+                    <Stack.Screen name="SignUp" component={SignUp} />
+                    <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+                    <Stack.Screen name="TasksMainScreen" component={TasksMainScreen} />
+                    <Stack.Screen
+                        name="OverdueTasksScreen"
+                        component={OverdueTasksScreen}
+                    />
+                    <Stack.Screen name="AddTask" component={AddTask} />
+                    <Stack.Screen name="Goals" component={Goals} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </AlertProvider>
+    );
 }
